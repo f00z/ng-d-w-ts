767504a19d750207311980a94fc76ec480a0cf97
Fahad Zia <fahzia@gmail.com>
2019-09-17 17:12:21 -0500

    save prior running prettier on the project

--- ngAuction/src/app/shared/product.service.ts
+++ ngAuction/src/app/shared/product.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable
+} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class Product {
+    constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public description: string,
+    public categories: string[]) {}
+}
 
-  constructor() { }
+const products = [{
+    id: 0,
+    title: 'First Product',
+    price: 24.99,
+    rating: 4.3,
+    description: 'This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categories: ['electronics', 'hardware']
+  },
+  {
+    id: 1,
+    title: 'Second Product',
+    price: 64.99,
+    rating: 3.5,
+    description: 'This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categories: ['books']
+  }];
+
+export class ProductService {
+  getProducts(): Product[] {
+    return products.map(p => new Product(
+      p.id,
+      p.title,
+      p.price,
+      p.rating,
+      p.description,
+      p.categories));
+  }
+
+  getProductById(productId: number): Product {
+    return products.find(p => p.id === productId);
+  }
 }
